317ff584cd2f540c32def4ceb98578bc6d46607c
锐 朱 <nicesiri4268@126.com>
2020-05-27 21:03:54 +0800

    整理代码8

--- src/com/MysqTest/Demo.java
+++ src/com/MysqTest/Demo.java
@@ -4,9 +4,6 @@ import com.Mysql.DataTable;
 import com.Mysql.MysqlConnect;
 import com.Mysql.MysqlMessage;
 import com.Mysql.MysqlResultManage;
-
-import java.lang.reflect.Type;
-import java.sql.Connection;
 import java.sql.Types;
 import java.util.Scanner;
 
@@ -23,9 +20,11 @@ public class Demo {
 
     public static void main(String[] args) {
         Demo demo = new Demo();
-        //demo.SearchSQL();
         demo.insertSQL();
-
+        demo.SearchSQL();
+        demo.updateSQL();
+        demo.SearchSQL();
+        demo.deleteSQL();
     }
 
     private static void showRowValues(DataTable dataTable) {
@@ -59,12 +58,17 @@ public class Demo {
         String user = input.nextLine();
         System.out.print("请输入密码： ");
         String passwd = input.nextLine();
-        String searchSQL = "select * from student";
+        String searchSQL = "select * from studenttest";
         MysqlMessage mysqlMessage = new MysqlMessage("test", user, passwd);
+        //设置访问的数据库，用户名，密码
         MysqlConnect mysqlConnect = new MysqlConnect(mysqlMessage);
+        //构建连接类
         mysqlConnect.setPstmtParam(searchSQL, null, null);
+        //预编译
         MysqlResultManage mysqlResultManage = new MysqlResultManage(mysqlConnect.getResultType());
+        //构建结果类
         DataTable dataTable = mysqlResultManage.manageResult();
+        //返回赋好值的结果表
         showColumnName(dataTable);
         showRowValues(dataTable);
     }
@@ -75,15 +79,48 @@ public class Demo {
         String user = input.nextLine();
         System.out.print("请输入密码： ");
         String passwd = input.nextLine();
-        String searchSQL = "insert into studenttest(Sno, Sname, Ssex, Sage, Sdept)values(?,?,?,?,?)";
+        String insertSQL = "insert into studenttest(Sno, Sname, Ssex, Sage, Sdept)values(?,?,?,?,?)";
         MysqlMessage mysqlMessage = new MysqlMessage("test", user, passwd);
         MysqlConnect mysqlConnect = new MysqlConnect(mysqlMessage);
-        mysqlConnect.setPstmtParam(searchSQL,
+        mysqlConnect.setPstmtParam(insertSQL,
                 new int[]{Types.CHAR, Types.CHAR, Types.CHAR, Types.SMALLINT, Types.CHAR},
-                new String[]{"201215127", "王五", "男", "20", "MS"});
+                new String[]{"201215128", "张三", "男", "20", "MS"});
         MysqlResultManage mysqlResultManage = new MysqlResultManage(mysqlConnect.getResultType());
+        //调用结果集的getresultCount方法返回修改的行数
         System.out.println(mysqlResultManage.getResultCount());
 
 
+    }
+
+    public void deleteSQL() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入用户名：");
+        String user = input.nextLine();
+        System.out.print("请输入密码： ");
+        String passwd = input.nextLine();
+        String deleteSQL = "delete from Studenttest where Sno = ?";
+        MysqlMessage mysqlMessage = new MysqlMessage("test", user, passwd);
+        MysqlConnect mysqlConnect = new MysqlConnect(mysqlMessage);
+        mysqlConnect.setPstmtParam(deleteSQL,
+                new int[]{Types.CHAR},
+                new String[]{"201215127"});
+        MysqlResultManage mysqlResultManage = new MysqlResultManage(mysqlConnect.getResultType());
+        System.out.println(mysqlResultManage.getResultCount());
+    }
+
+    public void updateSQL() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("请输入用户名：");
+        String user = input.nextLine();
+        System.out.print("请输入密码： ");
+        String passwd = input.nextLine();
+        String updateSQL = "update Studenttest set Sname = ? where Sno = ?";
+        MysqlMessage mysqlMessage = new MysqlMessage("test", user, passwd);
+        MysqlConnect mysqlConnect = new MysqlConnect(mysqlMessage);
+        mysqlConnect.setPstmtParam(updateSQL,
+                new int[]{Types.CHAR, Types.CHAR},
+                new String[]{"张三", "201215127"});
+        MysqlResultManage mysqlResultManage = new MysqlResultManage(mysqlConnect.getResultType());
+        System.out.println(mysqlResultManage.getResultCount());
     }
 }
